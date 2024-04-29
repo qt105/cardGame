dda099d5e74425cef8e11cfc3890ba1df2af86da
quentin mireur <qmireur@gmail.com.com>
2024-04-29 15:34:24 +0200

    added scripts.js ans cardList.js

---
+++ cardList.js
@@ -0,0 +1,22 @@
+let cards = [
+    {
+        name:'Croc Féroce',
+        type:'Attack',
+        image:'content/images/crocFeroce.jpeg',
+        cost: 2,
+        damage: 3,
+        weaken: 1,
+        shield: 0,
+        heal: 0
+    },
+    {
+        name:'Bouclier basique',
+        type:'Defense',
+        image:'content/images/bouclier.jpeg',
+        cost: 3,
+        damage: 0,
+        weaken: 0,
+        shield: 5,
+        heal: 0
+    }
+    ]
---
+++ scripts.js
@@ -0,0 +1,36 @@
+function typeColor(cardType){
+    switch (cardType) {
+        case 'Attack':
+            return '#f8d5d5'
+        
+        case 'Defense':
+            return '#D5E1F8'
+
+        case 'Healing':
+            return 'green'
+        default:
+            return 'white'
+    }
+}
+
+function getContent(selectedCard) {
+    let contentHTML = '';
+
+        if (selectedCard.damage > 0) {
+            contentHTML += `<p>Attaque et inflige <span class="attackColor">${selectedCard.damage} points de dégâts physiques</span></p>`;
+        }
+
+        if (selectedCard.weaken > 0) {
+            contentHTML += `<p>L'attaque octroie <span class="weaknessInflictColor">${selectedCard.weaken} point de faiblesse à l'adversaire</span></p>`;
+        }
+
+        if (selectedCard.shield > 0) {
+            contentHTML += `<p>La carte offre <span class="shieldColor" >${selectedCard.shield} points de bouclier</span></p>`;
+        }
+
+        if (selectedCard.heal > 0) {
+            contentHTML += `<p>La carte restaure <span class="shieldColor" >${selectedCard.shield} points de vie</span></p>`;
+        }
+
+        return contentHTML;
+}
